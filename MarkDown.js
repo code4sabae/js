5823b65931f177ade0d796ec9ceea93926fa520c
Taisuke Fukuno <fukuno@jig.jp>
2020-08-20 10:36:14 +0900

    add mark-down tag

---
+++ MarkDown.js
@@ -0,0 +1,11 @@
+import { marked } from "./marked.min.es.js";
+
+class MarkDown extends HTMLElement {
+  constructor() {
+    super();
+    this.innerHTML = marked(this.textContent);
+  }
+}
+customElements.define("mark-down", MarkDown);
+
+export { MarkDown };
