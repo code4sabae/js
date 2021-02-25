56dd862b73750cef012b536ed445840ce6b81825
Taisuke Fukuno <fukuno@jig.jp>
2021-02-25 11:48:05 +0900

    fix fetchJSON

--- fetchJSON.js
+++ fetchJSON.js
@@ -1,11 +1,11 @@
 const fetchJSON = async (url, req) => {
-  const opt = {
+  const opt = req ? {
     method: "POST",
     mode: "cors",
     cache: "no-cache",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(req),
-  };
+  } : null;
   const res = await (await fetch(url, opt)).json();
   return res;
 };
