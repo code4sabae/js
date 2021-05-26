5b771cfd55b2bfeff2d0614135ee52d22366a0bb
Taisuke Fukuno <fukuno@jig.jp>
2021-05-27 07:47:48 +0900

    improve waitClick

--- waitClick.js
+++ waitClick.js
@@ -1,11 +1,36 @@
-const waitClick = async (c) => {
-  return new Promise((resolve) => {
-    const f = () => {
-      c.removeEventListener("click", f);
-      resolve();
-    };
-    c.addEventListener("click", f);
-  });
+const waitClick = (comp) => {
+  comp ||= document.body;
+  if (!Array.isArray(comp)) {
+    return new Promise((resolve) => {
+      comp.style.cursor = "pointer";
+      const f = () => {
+        comp.removeEventListener("click", f);
+        comp.style.cursor = "";
+        resolve();
+      };
+      comp.addEventListener("click", f);
+    });
+  } else {
+    return new Promise((resolve) => {
+      for (const c of comp) {
+        c.style.cursor = "pointer";
+        const f = () => {
+          let idx = 0;
+          let res = 0;
+          for (const c2 of comp) {
+            c2.removeEventListener("click", f);
+            c2.style.cursor = "";
+            if (c == c2) {
+              res = idx;
+            }
+            idx++;
+          }
+          resolve(res);
+        };
+        c.addEventListener("click", f);
+      }
+    });
+  }
 };
 
 export { waitClick };
