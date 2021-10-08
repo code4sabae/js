2e0f4f14c379379edc886d76868457ac08189c3c
Taisuke Fukuno <fukuno@jig.jp>
2021-10-09 07:29:37 +0900

    add fetchBinPOST

--- fetchPOST.js
+++ fetchPOST.js
@@ -1,12 +1,19 @@
-const fetchPOST = async (url, param) => {
+const fetchRawPOST = async (url, param) => {
   const method = "POST";
   const body = Object.keys(param).map((s) => s + "=" + encodeURIComponent(param[s])).join("&");
   const headers = {
     'Accept': 'application/json',
     'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'
   };
-  return await (await fetch(url, { method, headers, body })).json();
+  return await fetch(url, { method, headers, body });
 };
 
+const fetchPOST = async (url, param) => {
+  return await (await fetchRawPOST(url, param)).json();
+}
 
-export { fetchPOST };
+const fetchBinPOST = async (url, param) => {
+  return new Uint8Array(await (await fetchRawPOST(url, param)).arrayBuffer());
+}
+
+export { fetchPOST, fetchBinPOST, fetchRawPOST };
