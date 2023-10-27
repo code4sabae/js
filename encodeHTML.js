6345a787262cd952ea532303898537c0c68348d6
Taisuke Fukuno <fukuno@jig.jp>
2023-10-28 04:11:50 +0900

    add withlink param

--- encodeHTML.js
+++ encodeHTML.js
@@ -1,16 +1,21 @@
-export const encodeHTML = (s) => { // supported: string, array, object
+export const addLink = (s) => {
+  const reg = /((https?:\/\/[a-zA-Z0-9.\-_@:/~?%&;=+#',()*!\[\]]+))/g;
+  return s.replace(reg, (all, url) => `<a href="${url}">${url}</a>`);
+};
+
+export const encodeHTML = (s, withlink) => { // supported: string, array, object
   if (s == null) {
     return null;
   }
   if (Array.isArray(s)) {
-    return s.map(s => encodeHTML(s));
+    return s.map(s => encodeHTML(s, withlink));
   }
   const t = typeof s;
   if (t == "object") {
     const res = {};
     for (const name in s) {
-      const n2 = encodeHTML(name);
-      const v2 = encodeHTML(s[name]);
+      const n2 = encodeHTML(name, withlink);
+      const v2 = encodeHTML(s[name], withlink);
       res[n2] = v2;
     }
     return res;
@@ -22,5 +27,8 @@ export const encodeHTML = (s) => { // supported: string, array, object
   s = s.replace(/</g, "&lt;");
   s = s.replace(/>/g, "&gt;");
   s = s.replace(/\n/g, "<br>");
+  if (withlink) {
+    s = addLink(s);
+  }
   return s;
 };
--- test/encodeHTML.test.js
+++ test/encodeHTML.test.js
@@ -22,3 +22,14 @@ Deno.test("object", () => {
 Deno.test("null", () => {
   t.assertEquals(encodeHTML(null), null);
 });
+Deno.test("withlink", () => {
+  t.assertEquals(encodeHTML("https://sabatwi.com/", true), `<a href="https://sabatwi.com/">https://sabatwi.com/</a>`);
+  t.assertEquals(encodeHTML("https://sabatwi.com/"), `https://sabatwi.com/`);
+  t.assertEquals(encodeHTML("https://sabatwi.com/", false), `https://sabatwi.com/`);
+  t.assertEquals(encodeHTML("ahttps://sabatwi.com/", true), `a<a href="https://sabatwi.com/">https://sabatwi.com/</a>`);
+  t.assertEquals(encodeHTML("https://sabatwi.com/ https://sabatwi.com/", true), `<a href="https://sabatwi.com/">https://sabatwi.com/</a> <a href="https://sabatwi.com/">https://sabatwi.com/</a>`);
+  t.assertEquals(encodeHTML("http://sabatwi.com/", true), `<a href="http://sabatwi.com/">http://sabatwi.com/</a>`);
+  t.assertEquals(encodeHTML("http://sabatwi.com:80/", true), `<a href="http://sabatwi.com:80/">http://sabatwi.com:80/</a>`);
+  t.assertEquals(encodeHTML("http://[::]:80/", true), `<a href="http://[::]:80/">http://[::]:80/</a>`);
+  t.assertEquals(encodeHTML("http://sabatwi.com/あ", true), `<a href="http://sabatwi.com/">http://sabatwi.com/</a>あ`);
+});
